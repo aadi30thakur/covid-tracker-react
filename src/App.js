069e49163f9580e33059feb98ed3b998169d7810
aditya thakur <aadi30thakur@github.com>
2020-08-10 10:30:44 +0530

    get data in seprate file

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React, { Component } from "react";
-import axios from "axios";
-const API = "https://api.covid19api.com/summary";
+import GetData from "./getData";
+
 // const DEFAULT_QUERY = "redux";
 
 class App extends Component {
-  state = {
-    contries: [],
-    global: [],
-    currentDate: [],
-    loading: true,
-  };
-
-  async componentDidMount() {
-    const response = await axios.get(API);
-    console.log(response);
-    this.setState({
-      countries: response.data.Countries,
-      global: response.data.global,
-      currentDate: response.data.currentDate,
-      loading: false,
-    });
-  }
   render() {
-    if (this.state.loading) {
-      return <h1>loading...</h1>;
-    }
-    return <div>hello</div>;
+    return (
+      <div>
+        <GetData />
+      </div>
+    );
   }
 }
 export default App;
--- src/getData.js
+++ src/getData.js
@@ -1,10 +1,27 @@
-export const GlobalData = () => {
-  return fetch("https://api.covid19api.com/summary", {
-    method: "GET",
-  })
-    .then((response) => {
-      console.log(response);
-      return response.json();
-    })
-    .catch((err) => console.log(err));
-};
+import React, { Component } from "react";
+import Axios from "axios";
+const API = "https://api.covid19api.com/summary";
+class GetData extends Component {
+  state = {
+    contries: [],
+    global: [],
+    currentDate: [],
+    loading: true,
+  };
+  async componentDidMount() {
+    const res = await Axios.get(API);
+    console.log(res);
+    this.setState({
+      countries: res.data.Countries,
+      global: res.data.global,
+      currentDate: res.data.currentDate,
+      loading: false,
+    });
+  }
+
+  render() {
+    return <div>hello</div>;
+  }
+}
+
+export default GetData;
