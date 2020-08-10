6ae5994e289a1dc5ba4c5c6cec0967bc8790a041
aditya thakur <aadi30thakur@github.com>
2020-08-10 17:01:06 +0530

    adding date

--- src/core/Dashboard.js
+++ src/core/Dashboard.js
@@ -1,15 +1,61 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Navbar from "./Navbar";
-
 class Dashboard extends Component {
   render() {
     const { summary, currentDate } = this.props;
     return (
-      <div>
+      <Fragment>
         <Navbar />
-        <div className="text-center text-white">{summary.NewConfirmed}</div>
-        <div>{currentDate}</div>
-      </div>
+        <h1 className="text-center text-white font h1"> World data</h1>
+        <div className="row container mx-auto">
+          <div className="col-md-4">
+            <h3 className="text-info font">New Confirmed Cases</h3>
+          </div>
+          <div className="col-md-4">
+            <h3 className="text-danger font">New Death Cases</h3>
+          </div>
+          <div className="col-md-4">
+            <h3 className="text-success font">Recovered in last 24 hours</h3>
+          </div>
+          {/* <div className="col-md-4">
+            <h3 className="text-white font">Total Confirmed Cases</h3>
+          </div>
+          <div className="col-md-4">
+            <h3 className="text-white font">Total Deaths</h3>
+          </div>
+          <div className="col-md-4">
+            <h3 className="text-white font">Total Recovered</h3>
+          </div> */}
+        </div>
+
+        <div className="row container mx-auto">
+          <div className="col-md-4">
+            <h3 className="text-white font">{summary.NewConfirmed}</h3>
+          </div>
+          <div className="col-md-4">
+            <h3 className="text-white font">{summary.NewDeaths}</h3>
+          </div>
+          <div className="col-md-4">
+            <h3 className="text-white font">{summary.NewRecovered}</h3>
+          </div>
+          {/* <div className="col-md-4">
+            <h3 className="text-white font">{summary.TotalConfirmed}</h3>
+          </div>
+          <div className="col-md-4">
+            <h3 className="text-white font">{summary.TotalDeaths}</h3>
+          </div>
+          <div className="col-md-4">
+            <h3 className="text-white font">{summary.TotalRecovered}</h3>
+          </div> */}
+        </div>
+        <h1 className="text-white font text-center h2">
+          dated :{" "}
+          <span className="text-warning">
+            {" "}
+            {new Date(currentDate).toDateString()}
+          </span>
+        </h1>
+      </Fragment>
     );
   }
 }
--- src/core/Navbar.js
+++ src/core/Navbar.js
@@ -3,31 +3,17 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        Navbar
+    <nav className="navbar navbar-expand-lg">
+      <Link className="navbar-brand text-white" to="/">
+        Home
       </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" href="/about">
-              about
-            </a>
-          </li>
-        </ul>
-      </div>
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a className="nav-link text-white" href="/about">
+            about
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 };
